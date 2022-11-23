65b9ff38a2b11c425f9186b842c94f50f68e8829
AkioSe7en <akiose7en@bk.ru>
2022-11-24 00:08:53 +0700

    Сделана проверка "Создана ли база данных"

--- Laba 3/MainWindow.xaml.cs
+++ Laba 3/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿
 using Laba3;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Laba3_wpf
 {
@@ -16,46 +18,95 @@ namespace Laba3_wpf
         
       private void Z1_OnClick(object sender, RoutedEventArgs e)
       {
-         var z1 = db.Cars.Where(p => p.Name == "Alfa Romeo").Where(p => p.IsStock == true).ToList();
-         Table1.ItemsSource = z1;
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
+              var z1 = db.Cars.Where(p => p.Name == "Alfa Romeo").Where(p => p.IsStock == true).ToList(); 
+              Table1.ItemsSource = z1;
+             }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
       }
 
       private void Z2_OnClick(object sender, RoutedEventArgs e)
       {
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
           var z2 = db.Cars.Where(p => p.Name.Contains("BMW")).Select(p => p.Stock).Distinct().ToList();
           Table1.ItemsSource = z2;
+          }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
       }
 
       private void Z3_OnClick(object sender, RoutedEventArgs e)
       {
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
           var z3 = db.Cars.Where(p => p.Cost < 10000).ToList();
           Table1.ItemsSource = z3;
+          }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
       }
 
       private void Z4_OnClick(object sender, RoutedEventArgs e)
       {
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
           var z4 = db.Cars.Where(p => p.Remark != "").OrderBy(p => p.Name).ToList();
           Table1.ItemsSource = z4;
+          }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
 
       }
 
       private void Z5_OnClick(object sender, RoutedEventArgs e)
       {
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
           var z5 = db.Cars.Where(p => p.DataRelease >= 2000 && p.DataRelease <= 2005).GroupBy(c => c.Stock.Town).Select(g => new { Name = g.Key, Count = g.Count() }).ToList();
           Table1.ItemsSource = z5;
+          }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
       }
 
       private void Z6_OnClick(object sender, RoutedEventArgs e)
       {
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
           var z6 = db.Cars.Where(p => p.DataRelease < 2000).OrderBy(p => p.DataRelease).ToList();
           Table1.ItemsSource = z6;
+          }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
       }
 
       private void Z7_OnClick(object sender, RoutedEventArgs e)
       {
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
           DbReport DBRep = new DbReport() { DateBase = db };
           DBRep.WriteAllReport();
           MessageBox.Show("Файл успешно создан");
+          }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
       }
 
       private void CreateBD_OnClick(object sender, RoutedEventArgs e)
@@ -68,7 +119,8 @@ namespace Laba3_wpf
               new() { Town = "Санкт-Петербург" }
           };
           var cars = CarGenerator.GetCars(stocks);
-
+          ApplicationContext db = new ApplicationContext();  
+          
           db.Database.EnsureDeleted();
           db.Database.EnsureCreated();
 
@@ -80,8 +132,15 @@ namespace Laba3_wpf
 
       private void WriteBD_OnClick(object sender, RoutedEventArgs e)
       {
+          if (db.Database.GetService<IRelationalDatabaseCreator>().HasTables())
+          { 
           var WriteAllDB = db.Cars.Include(p => p.Stock).ToList();
           Table1.ItemsSource = WriteAllDB;
+          }
+          else
+          {
+              MessageBox.Show("БД не создана");
+          }
       }
     }
     }
