c1b6c17fd10928ff51ac3ac4d8c7d6150bb58fa4
AkioSe7en <akiose7en@bk.ru>
2022-11-23 23:15:52 +0700

    Создание проекта

---
+++ Laba 3/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Laba_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
